dc46d339e5c9c2cb61209684fc127b38c157ba4c
Gregory Lureau <gregorylureau@gmail.com>
2016-04-24 15:46:28 +0200

    Also use unused methods in main to show full implementation.

--- src/glureau/kdp/adapter/java/Demo.java
+++ src/glureau/kdp/adapter/java/Demo.java
@@ -11,8 +11,8 @@ public class Demo {
 		TargetInterface classAdapter = new ClassAdapter(3.2, 6.7);
 		System.out.println("Getting: operands through the Adaptee interface: " + adaptee.getFirstOperand() + " ; " + adaptee.getSecondOperand());
 		System.out.println("Getting: operands through the TargetInterface interface: " + classAdapter.getOperands());
-		System.out.println("Getting: operands sum through the Adaptee interface: " + adaptee.computeSum());
-		System.out.println("Getting: operands sum through the TargetInterface: " + classAdapter.sum());
+		System.out.println("Computing 'sum ; max' through the Adaptee interface: " + adaptee.computeSum() + " ; " + adaptee.max());
+		System.out.println("Computing 'sum ; max ; multiply' through the TargetInterface: " + classAdapter.sum() + " ; " + classAdapter.max() + " ; " + classAdapter.multiply());
 
 		System.out.println();
 		System.out.println("***Demo: pattern Adapter with object adaptation");
@@ -20,7 +20,7 @@ public class Demo {
 		TargetInterface objectAdapter = new ObjectAdapter(adaptee);
 		System.out.println("Getting: operands through the Adaptee interface: " + adaptee.getFirstOperand() + " ; " + adaptee.getSecondOperand());
 		System.out.println("Getting: operands through the TargetInterface interface: " + objectAdapter.getOperands());
-		System.out.println("Getting: operands sum through the Adaptee interface: " + adaptee.computeSum());
-		System.out.println("Getting: operands sum through the TargetInterface: " + objectAdapter.sum());
+		System.out.println("Computing 'sum ; max' through the Adaptee interface: " + adaptee.computeSum() + " ; " + adaptee.max());
+		System.out.println("Computing 'sum ; max ; multiply' through the TargetInterface: " + objectAdapter.sum() + " ; " + objectAdapter.max() + " ; " + objectAdapter.multiply());
 	}
 }
